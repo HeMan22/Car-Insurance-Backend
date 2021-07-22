fe3d8c1904da563a0814bcb942ab9c6493404900
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-07-23 02:47:33 +0530

    Added DriverServiceController class and just checking the ping

--- src/main/java/com/capstone/carInsurance/controllers/DriverServiceController.java
+++ src/main/java/com/capstone/carInsurance/controllers/DriverServiceController.java
@@ -1,19 +1,29 @@
 package com.capstone.carInsurance.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capstone.carInsurance.services.DriverService;
+import com.capstone.carInsurance.utility.APIResponse;
 
 @RestController
 @RequestMapping("/api/v1/carInsurance")
 public class DriverServiceController {
 
 	DriverService driverService;
-	
+	@Autowired
 	public DriverServiceController(DriverService driverService) {
-		super();
 		this.driverService=driverService;
+	}
+	
+	@GetMapping("/ping")
+	public ResponseEntity<APIResponse> getConnectionTest(){
+		APIResponse connectionTest = new APIResponse("SUCCESS","CAR Insurance API is Running",null);
+		return new ResponseEntity<APIResponse>(connectionTest,HttpStatus.OK);
 	}
 	
 }
