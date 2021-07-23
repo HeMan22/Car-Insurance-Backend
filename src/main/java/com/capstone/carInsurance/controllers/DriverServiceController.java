a72e3f1f375dbad067cfb6010c4c25aca14bd12a
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-07-23 23:54:16 +0530

    Created the API for Saving the Details of The Driver

--- src/main/java/com/capstone/carInsurance/controllers/DriverServiceController.java
+++ src/main/java/com/capstone/carInsurance/controllers/DriverServiceController.java
@@ -10,8 +10,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capstone.carInsurance.dto.DriverDTO;
+import com.capstone.carInsurance.model.Driver;
 import com.capstone.carInsurance.services.DriverService;
 import com.capstone.carInsurance.utility.APIResponse;
+import com.capstone.carInsurance.utility.DriverDTOConvertor;
 
 @RestController
 @RequestMapping("/api/v1/carInsurance")
@@ -31,9 +33,16 @@ public class DriverServiceController {
 	
 	@PostMapping("/save/driver")
 	public ResponseEntity<APIResponse> saveDriverInfo(@RequestBody DriverDTO driverDTO){
-				
-		APIResponse saveDriver = new APIResponse("SUCCESS", "Driver Saved",null);
 		
-		return new ResponseEntity<APIResponse>(saveDriver, HttpStatus.CREATED);
+		//Converting DTO to Driver Entity
+		Driver newDriverObj = DriverDTOConvertor.driverDtoToEntity(driverDTO);
+		
+		//Calling DriverServices to save the Driver Info
+		Driver saveDriverInfo = driverService.saveDriver(newDriverObj);
+		
+		// Setting API Response		
+		APIResponse saveDriverResponse = new APIResponse("SUCCESS", "Driver Saved",saveDriverInfo);
+		
+		return new ResponseEntity<APIResponse>(saveDriverResponse, HttpStatus.CREATED);
 	}
 }
