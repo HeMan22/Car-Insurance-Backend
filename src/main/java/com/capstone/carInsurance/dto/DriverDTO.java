e37721d393a4f278608f7224fe7f695a087fca40
Himanshu Tripathi <heman.tri2208@gmail.com>
2021-07-28 23:49:20 +0530

    Added Validations to DTO fields

--- src/main/java/com/capstone/carInsurance/dto/DriverDTO.java
+++ src/main/java/com/capstone/carInsurance/dto/DriverDTO.java
@@ -1,5 +1,8 @@
 package com.capstone.carInsurance.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,21 +14,38 @@ public class DriverDTO {
 
 	private Long driverID;
 	private String prefix;
+	@NotNull
 	private String firstName;
+	@NotNull
 	private String lastName;
+	@NotNull
 	private String contactNo;
+	@NotNull
 	private String email;
-	
+
 	private String addressLine1;
 	private String addressLine2;
 	private String city;
 	private String postCode;
-	
+
+	@NotNull
+	@NotEmpty
 	private String vehicleType;
+	@NotNull
+	@NotEmpty
 	private String engineSize;
+	@NotNull
+	@NotEmpty
 	private Long currentValue;
+	@NotNull
+	@NotEmpty
 	private int additionalDriver;
+	@NotNull
+	@NotEmpty
 	private String commercialUse;
+	@NotNull
+	@NotEmpty
 	private String outsideState;
+
 	private String registeredDate;
 }
